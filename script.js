d62f9e067ad119e4339b36bdd9add413b00ddfc5
Ellexandrei Esponilla <xandrei37@gmail.com>
2024-05-21 22:10:04 +0800

    suggestion

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
+const firstName = document.getElementById("firstName");
+const lastName = document.getElementById("lastName");
+const birthday = document.getElementById("birthday");
+const gender = document.getElementById("gender");
+const emailAddress = document.getElementById("emailAddress");
+const password = document.getElementById("Password");
+const confirmPassword = document.getElementById("confirmPassword");
 
+const firstName_error = document.getElementById("firstName_error");
 
-const firstName = document.getElementById('firstName');
-const lastName = document.getElementById('lastName');
-const birthday = document.getElementById('birthday');
-const gender = document.getElementById('gender');
-const emailAddress = document.getElementById('emailAddress');
-const password = document.getElementById('Password');
-const confirmPassword = document.getElementById('confirmPassword');
+console.log(firstName.innerHTML);
 
-const firstName_error = document.getElementById('firstName_error');
-
-FormData.addEventListener('submit', (e)=>
-{
-    if (firstName.value === '' || firstName.value == null)
-        {
-            e.preventDefault();
-            firstName_error.innerHTML = "Name is required";        }
-})
+function submitForm() {
+  if (!firstName.value) {
+    firstName_error.innerHTML = "Name is required";
+    return false;
+  }
+}
